5995cedecf25369b62bc89e16deec57c8d08d31d
Omar Khan <42219936+heyitsOK@users.noreply.github.com>
2025-08-06 18:39:36 -0400

    [fix] remove comment

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -23,7 +23,6 @@ export default async function Projects() {
       },
     },
   })
-  // const currentProjects = currentRes.docs
 
   const resGlobal = await payload.findGlobal({
     slug: 'projectsPage',
