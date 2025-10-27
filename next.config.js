b4080a021099a9bdac836c56d46726d4d47763e4
Omar Khan <omark212378@gmail.com>
2025-10-26 21:01:42 -0400

    testing removed everything

--- next.config.js
+++ next.config.js
@@ -32,9 +32,9 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   redirects,
-  env: {
-    NEXT_PUBLIC_SERVER_URL: process.env.DEPLOY_PRIME_URL,
-  },
+  // env: {
+  //   NEXT_PUBLIC_SERVER_URL: process.env.DEPLOY_PRIME_URL,
+  // },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
